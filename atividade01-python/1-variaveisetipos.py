d8a7df364cf50f19a738918ff0142d9e5a8cb9e0
Milena Leme <milenaleme4@hotmail.com>
2024-08-11 23:43:50 -0300

    primeira atividade de python

---
+++ atividade01-python/1-variaveisetipos.py
@@ -0,0 +1,55 @@
+'''
+1.1. Crie variáveis para armazenar o nome, idade, peso e altura de uma pessoa.
+Imprima os valores e os tipos de dados dessas variáveis.
+'''
+
+nome = "Milena"
+print(nome)
+print(type(nome))
+
+idade = 30
+print(idade)
+print(type(idade))
+
+peso = 77.0
+print(peso)
+print(type(peso))
+
+altura = 1.52
+print(altura)
+print(type(altura))
+
+"""
+resposta - terminal
+
+Milena
+<class 'str'>
+30
+<class 'int'>
+77.0
+<class 'float'>
+1.52
+<class 'float'>
+
+
+"""
+
+'''
+1.2. Atualize a variável de peso para um valor diferente e imprima o novo valor.
+'''
+
+peso = 70.0
+print(peso)
+
+"""
+Milena
+<class 'str'>
+30
+<class 'int'>
+77.0
+<class 'float'>
+1.52
+<class 'float'>
+70.0
+
+"""
