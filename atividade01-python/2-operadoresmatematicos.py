f103295b2da0c83530620be7f930949182ebb262
Milena Leme <milenaleme4@hotmail.com>
2024-08-11 23:44:45 -0300

    segunda atividade de python

---
+++ atividade01-python/2-operadoresmatematicos.py
@@ -0,0 +1,34 @@
+'''
+2.1. Crie uma variável a com valor 10 e uma variável b com valor 3. Realize as
+seguintes operações e imprima os resultados:
+Soma
+Subtração
+Multiplicação
+Divisão
+Parte inteira da divisão
+Resto da divisão
+Exponenciação
+'''
+
+a = 10
+b = 3
+
+print(a + b) # Soma
+print(a - b) # Subtração
+print(a * b) # Multiplicação
+print(a / b) # Divisão
+print(a // b) # Parte inteira da divisão
+print(a % b) # Resto da divisão
+print(a ** b) # Exponenciação
+
+"""
+
+13
+7
+30
+3.3333333333333335
+3
+1
+1000
+
+"""
