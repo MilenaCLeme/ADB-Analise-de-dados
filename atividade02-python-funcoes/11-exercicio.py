4148e3d1b263ad95115e6d8c1144b58d064189dc
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:15:00 -0300

    atividade python - exercicio 11

---
+++ atividade02-python-funcoes/11-exercicio.py
@@ -0,0 +1,19 @@
+"""
+11. **Função Anônima**:
+    - Crie uma função anônima e atribua-a a uma variável chamada `multiplicar`. A função deve receber dois números como argumentos e retornar a multiplicação deles.
+
+"""
+
+multiplicar = lambda a,b: a * b
+
+print(multiplicar(3,4))
+print(multiplicar(8,8))
+print(multiplicar(4,7))
+
+"""
+
+12
+64
+28
+
+"""
