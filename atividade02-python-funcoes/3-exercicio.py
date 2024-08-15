3d5230ffd4c26f40a13339ed6306682b5511e712
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:08:26 -0300

    atividade 2 - python - 3 exercicio

---
+++ atividade02-python-funcoes/3-exercicio.py
@@ -0,0 +1,24 @@
+"""
+
+
+3. **Função de Subtração**:
+   - Crie uma função chamada `subtracao` que receba dois números como argumentos e retorne a subtração do segundo número pelo primeiro.
+
+"""
+
+def subtracao(a, b):
+    return b-a
+
+print(subtracao(2,5)) 
+print(subtracao(10,50))
+print(subtracao(8,0))
+print(subtracao(5,9))
+
+"""
+
+3
+40
+-8
+4
+
+"""
