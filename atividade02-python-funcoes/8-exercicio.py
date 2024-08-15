fe73b0ed467839b92259d198add002b047e37def
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:12:41 -0300

    atividade 2 - python - exercicio 8

---
+++ atividade02-python-funcoes/8-exercicio.py
@@ -0,0 +1,25 @@
+"""
+
+
+8. **Função para Calcular Média**:
+   - Crie uma função chamada `media` que receba um array de números como argumento e retorne a média dos números.
+
+
+"""
+
+def media(lst):
+    return sum(lst) / len(lst)
+
+print(media([2,5,4,3,5]))
+print(media([6,8,12,15]))
+print(media([-2,13,17,4]))
+print(media([7,8,9]))
+
+"""
+
+3.8
+10.25
+8.0
+8.0
+
+"""
