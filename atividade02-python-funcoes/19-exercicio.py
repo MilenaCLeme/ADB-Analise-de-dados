932d08eb197e6aac2a185cb9bd97e6c87df34ec0
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:20:33 -0300

    atividade 02 - exercicio 19

---
+++ atividade02-python-funcoes/19-exercicio.py
@@ -0,0 +1,21 @@
+"""
+19. **Função para Calcular Quadrado**:
+    - Crie uma função chamada `quadrado` que receba um número como argumento e retorne o quadrado desse número.
+"""
+
+def quadrado(n):
+    return n ** n
+
+print(quadrado(5))
+print(quadrado(8))
+print(quadrado(9))
+print(quadrado(5))
+
+"""
+
+3125
+16777216
+387420489
+3125
+
+"""
