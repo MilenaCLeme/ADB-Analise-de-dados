1f54fb7106915f63e9a48e00af14e3248a7ffe84
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:10:35 -0300

    atividade 2 - python - exercicio 6

---
+++ atividade02-python-funcoes/6-exercicio.py
@@ -0,0 +1,13 @@
+"""
+
+6. **Função de Fatorial**:
+   - Crie uma função chamada `fatorial` que receba um número como argumento e retorne o fatorial desse número.
+
+"""
+
+def fatorial(n):
+    return n * fatorial(n-1) if n > 1 else 1
+
+print(fatorial(5))
+print(fatorial(10))
+print(fatorial(6))
