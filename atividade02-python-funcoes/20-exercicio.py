0546640efe7820d41e0b89cb8a70b6ea71e09784
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:13:54 -0300

    atividade 2 - python - exercicio 20

---
+++ atividade02-python-funcoes/20-exercicio.py
@@ -0,0 +1,11 @@
+"""
+
+20. **Função para Calcular Raiz Quadrada**:
+    - Crie uma função chamada `raizQuadrada` que receba um número como argumento e retorne a raiz quadrada desse número.
+
+"""
+
+import math
+
+def raizQuadrada(n):
+    return math.sqrt(n)
