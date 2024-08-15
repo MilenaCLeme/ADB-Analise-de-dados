00a3200df0a7b08b244596e5558f398d0b98906d
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:07:33 -0300

    atividade 2 - python - exercicio 2

---
+++ atividade02-python-funcoes/2-exercicio.py
@@ -0,0 +1,23 @@
+"""
+
+ **Função de Soma**:
+   - Crie uma função chamada `soma` que receba dois números como argumentos e retorne a soma deles.
+
+"""
+
+def soma(a, b):
+    return a+b
+
+print(soma(2,5)) 
+print(soma(10,50))
+print(soma(8,0))
+print(soma(5,9))
+
+"""
+
+7
+60
+8
+14
+
+"""
