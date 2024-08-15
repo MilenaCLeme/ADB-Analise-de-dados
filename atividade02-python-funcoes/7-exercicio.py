d1e300320a9c4692d453f3accc0a714064880711
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:11:16 -0300

    atividade 2 - python - exercicio 7

---
+++ atividade02-python-funcoes/7-exercicio.py
@@ -0,0 +1,6 @@
+"""
+
+7. **Função para Verificar Número Primo**:
+   - Crie uma função chamada `ePrimo` que receba um número como argumento e retorne `true` se o número for primo e `false` caso contrário.
+
+"""
