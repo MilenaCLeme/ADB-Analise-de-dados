9ee14464929d4831c5d0923d9c6862a729035424
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:13:23 -0300

    atividade 2 - python - exercicio 9

---
+++ atividade02-python-funcoes/9-exercicio.py
@@ -0,0 +1,22 @@
+"""
+
+9. **Função para Inverter String**:
+   - Crie uma função chamada `inverterString` que receba uma string como argumento e retorne a string invertida.
+
+"""
+
+import re
+
+def inverterString(frase):
+    return frase[::-1]
+
+print(inverterString('Milena'))
+print(inverterString('Você'))
+print(inverterString('Tudo isso é muito bom'))
+
+"""
+aneliM
+êcoV
+mob otium é ossi oduT
+
+"""
