da0981bc0eb8d478981d64f6c0e3673a639b83b5
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:20:03 -0300

    atividade 02 - exercicio 18

---
+++ atividade02-python-funcoes/18-exercicio.py
@@ -0,0 +1,22 @@
+"""
+
+
+18. **Função para Verificar Paridade**:
+    - Crie uma função chamada `ePar` que receba um número como argumento e retorne `true` se o número for par e `false` caso contrário.
+
+"""
+
+def ePar(n):
+    return n % 2 == 0
+
+print(ePar(2))
+print(ePar(5))
+print(ePar(75))
+
+"""
+
+True
+False
+False
+
+"""
