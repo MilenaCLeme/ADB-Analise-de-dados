e57edbcd7a69d46887e769eff9984b8f2814e7b0
Milena Leme <milenaleme4@hotmail.com>
2024-08-15 09:08:54 -0300

    atividade 2 - python - exercicio 4

---
+++ atividade02-python-funcoes/4-exercicio.py
@@ -0,0 +1,23 @@
+"""
+
+4. **Função de Multiplicação**:
+   - Crie uma função chamada `multiplicacao` que receba dois números como argumentos e retorne a multiplicação deles.
+
+"""
+
+def multiplicacao(a,b):
+    return a * b
+
+print(multiplicacao(2,5)) 
+print(multiplicacao(10,50))
+print(multiplicacao(8,0))
+print(multiplicacao(5,9))
+
+"""
+
+10
+500
+0
+45
+
+"""
