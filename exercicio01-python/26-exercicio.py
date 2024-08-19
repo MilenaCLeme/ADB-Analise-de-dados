3d04a4e625b36e71ac6492c2f22d99c5a6f04a11
Milena Leme <milenaleme4@hotmail.com>
2024-08-19 01:33:44 -0300

    exercicio 01 - python introducao - 26 exercicio

---
+++ exercicio01-python/26-exercicio.py
@@ -0,0 +1,8 @@
+"""
+26. Gerar Números Aleatórios
+    - **Descrição:** Gere e exiba uma lista de 10 números aleatórios entre 1 e 100. Use um loop `for` e a biblioteca `random` para isso.
+"""
+import random
+
+def gerar_numero_aleatorio_de_1_a_100():
+    return random.randint(1, 100)
